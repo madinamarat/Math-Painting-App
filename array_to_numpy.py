bc2e23e0d6e9e9c4b9a70c5f87fbe864985f806f
Madina Mukhambekova <madina.maratovna@gmail.com>
2021-09-27 15:28:52 +0200

    initial commit

---
+++ array_to_numpy.py
@@ -0,0 +1,4 @@
+import numpy as np
+from PIL import Image
+
+data=np.zeros((5,4,3),dtype=np.unit8)
---
+++ shapes.py
@@ -0,0 +1,25 @@
+class Rectangle:
+    """A rectangle shape that can be drawn on a Canvas object"""
+    def __init__(self,x,y,width,height,color):
+        self.color = color
+        self.y = y
+        self.x = x
+        self.height = height
+        self.width = width
+        self.color = color
+
+    def draw(self,canvas):
+        """Draws itself into the canvas"""
+        canvas.data[self.x:self.x+self.height, self.y:self.y+self.width]=self.color
+
+
+class Square:
+
+    def __init__(self,x,y,side,color):
+        self.x = x
+        self.y = y
+        self.side = side
+        self.color = color
+
+    def draw(self,canvas):
+        canvas.data[self.x:self.x+self.side, self.y:self.y+self.side]=self.color
